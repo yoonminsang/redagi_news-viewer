3b9e7b1348efe44ba5bbabc3fcac1e81fbe9db5e
yoonminsang <apdlyooapdl@naver.com>
2021-02-08 23:55:38 +0900

    ex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import NewsPage from "./pages/NewsPage";
+import { Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  // const [category, setCategory] = useState("all");
+  // const onSelect = useCallback((category) => setCategory(category), []);
+  return <Route path="/:category?" component={NewsPage} />;
+  // return (
+  //   <>
+  //     <Categories category={category} onSelect={onSelect} />
+  //     <NewsList category={category} />
+  //   </>
+  // );
+};
 
 export default App;
